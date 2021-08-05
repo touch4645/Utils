466fde5fecb95ac3f15be51aa15ac342be9e2c05
touch4645 <k.terazawa@lillyholdings.co.jp>
2021-08-05 18:29:47 +0900

    add transpose function

---
+++ utils.js
@@ -0,0 +1,11 @@
+/**
+ * このライブラリは使い回し可能なロジックのみを収集したものになります。
+ * 各スクリプト特有のビジネスロジックは含めないよう心がけてください。 
+ */
+
+/**
+ * 二次元配列を転置する関数
+ * @param {Array<Array<any>} a 二次元配列
+ * @return {Array<Array<any>} 転置された二次元配列
+ */
+function transpose(a) { console.log( a[0].map((_, c) => a.map(r => r[c])) )};
